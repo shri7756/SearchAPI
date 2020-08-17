87cc08297e8a4c66f11b2fc9fe1561cdbb9c77d7
sharansd <sharansd6@gmail.com>
2020-08-17 18:08:56 +0530

    validation commit

--- src/main/java/com/search/restservice/application/SearchApplication.java
+++ src/main/java/com/search/restservice/application/SearchApplication.java
@@ -1,8 +1,10 @@
 package com.search.restservice.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.search.restservice.controller"})
 public class SearchApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/search/restservice/controller/SearchController.java
+++ src/main/java/com/search/restservice/controller/SearchController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import com.search.restservice.request.SearchRequest;
 
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletRequest;
 
 
@@ -21,20 +23,50 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 public class SearchController {
-	//TODO Add the Appropriate logger and Exceptions
-
-    /**
-     * This method returns the search-nav results
-     *
-     * @param searchRequest
-     * @param headers
-     * @return
-     * @throws ApiException
-     */
     @RequestMapping(value = "/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<?> doPageSearch(@ModelAttribute SearchRequest searchRequest, BindingResult reqValidate,
                                           @RequestHeader HttpHeaders headers, final HttpServletRequest request) {
-        //return doSearchWithMetrics(searchRequest, reqValidate, headers, request, null);
+    	
+    	
+    	String keyword;
+    	String pageSize;
+    	String pageIndex;    	
+    	
+    	keyword=request.getParameter("keyword");
+    	pageSize=request.getParameter("pageSize");
+    	pageIndex=request.getParameter("pageIndex");
+    	
+    	if(keyword==null || keyword.isEmpty())
+    	{
+    		return new ResponseEntity<>("please enter the keyword",HttpStatus.BAD_REQUEST);
+    	}
+    	else if(keyword!=null){
+    		
+    		return new ResponseEntity<>("ok",HttpStatus.OK);
+    		
+    	}
+    	
+    	
+    	if(pageSize==null) {
+    		return new ResponseEntity<>("no page size",HttpStatus.BAD_REQUEST);
+    		
+    	}
+    	
+    	if(pageSize.isEmpty())
+    	{
+    		return new ResponseEntity<>("empty pagesize",HttpStatus.BAD_REQUEST);
+    	}
+    	
+    	
+    	Integer keynum=Integer.parseInt(keyword);
+    	Integer pagenum=Integer.parseInt(pageSize);
+    	 
+    	int[] array = Arrays.asList(pageIndex).stream().mapToInt(Integer::parseInt).toArray(); //convert String to integer array.
+    	
+    	
+    	
+    	
+    	
     	return new ResponseEntity<>("Well come to Basic Spring boot application", HttpStatus.OK); 
     }
 
